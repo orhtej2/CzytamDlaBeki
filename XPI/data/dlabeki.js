0a0637b104984cedb6cb5490dda517b1489acb3b
Mateusz Szymanski <orthej2@gmail.com>
2015-01-21 23:52:01 +0100

    Poprawiono współpracę ze stroną wpolityce.pl

--- XPI/data/dlabeki.js
+++ XPI/data/dlabeki.js
@@ -1,3 +1,4 @@
+(function() {
 var image_element = document.createElement('img');
 image_element.id = "dla-beki-ribbon";
 image_element.src = self.options.imageURL;
@@ -5,6 +6,8 @@ image_element.style.position = "fixed";
 image_element.style.top = "0px";
 image_element.style.right = "0px";
 image_element.style.border = "0px";
+image_element.style.zIndex = "10000";
 // delete itself upon clicking
 image_element.onclick = function() {document.body.removeChild(document.getElementById("dla-beki-ribbon"))};
 document.body.appendChild(image_element);
+}) ();
